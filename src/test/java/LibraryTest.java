e153aec344658eefc5742504b0fc38b5211d6242
David Aanestad <davidaan@stud.ntnu.no>
2019-02-09 01:08:58 +0100

    Git test

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-	//test
     @Test public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
