6cec16f22d18017c2ae549585d61114b31089820
David Aanestad <davidaan@stud.ntnu.no>
2019-03-14 18:11:17 +0100

    View evaluations temp

--- src/main/java/evaluation/EvaluationInbox.java
+++ src/main/java/evaluation/EvaluationInbox.java
@@ -7,13 +7,18 @@ import database.DBEvaluation;
 
 public class EvaluationInbox{
 	private String courseCode;
-	private HashMap<String, ArrayList<Evaluation>> evaluations = new HashMap<String, ArrayList<Evaluation>>();
+	private ArrayList<Evaluation> evaluations = new ArrayList<Evaluation>();
+	
+	public EvaluationInbox(String course) {
+		this.courseCode = course;
+		refresh();
+	}
 	
 	public void addEval(Evaluation evaluation) {
 		DBEvaluation.insertEvaluation(evaluation);
 	}
 
-	public HashMap<String, ArrayList<Evaluation>> getEvaluations() {
+	public ArrayList<Evaluation> getEvaluations() {
 		return evaluations;
 	}
 
--- src/main/java/gui/controllers/StudentViewAssignmentsPageController.java
+++ src/main/java/gui/controllers/StudentViewAssignmentsPageController.java
@@ -1,16 +1,93 @@
 package gui.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import evaluation.Assignment;
+import evaluation.AssignmentInbox;
+import evaluation.Evaluation;
+import evaluation.EvaluationInbox;
 import gui.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class StudentViewAssignmentsPageController {
 	
 	@FXML
+	TableView<Assignment> messageTable;
+	
+	@FXML
+	TableColumn<Assignment, String> assignmentCol;
+	
+	@FXML
+	TableColumn<Assignment, String> scoreCol;
+
+	
+	@FXML
+	ChoiceBox<String> course_input;
+	
+	@FXML
 	Button return_handler;
 	
+	EvaluationInbox inbox;
+	
+	@FXML
+	public void initialize() {
+		List<String> relevantCourses = findCoursesWhereStudent();
+		course_input.getItems().addAll(relevantCourses);
+		
+		loadAssignments();
+	}
+	
+	private void loadAssignments() {
+		assignmentCol.setCellValueFactory(new PropertyValueFactory<>("senderEmail"));
+		scoreCol.setCellValueFactory(new PropertyValueFactory<>("subject"));
+		
+		inbox = new EvaluationInbox(course_input.getValue());
+		ArrayList<Evaluation> evaluations = AssignmentInbox.getEvaluations(course_input.getValue());
+		
+		Logger.getLogger(App.class.getName()).log(Level.INFO, "Evaluations downloaded");
+		
+		evaluations = getEvaluationsWhereStudent(evaluations);
+		Collections.sort(messages);
+		
+		messageTable.getItems().addAll(messages);
+
+	}
+	
+	private ArrayList<Evaluation> getEvaluationsWhereStudent(ArrayList<Evaluation> evaluations) {
+		ArrayList<Evaluation> newList = new ArrayList<Evaluation>();
+		for (Evaluation evaluation : evaluations)
+			if (!evaluation.getEvaluator().getEmail().equals(App.getInstance().getLoggedUser().getEmail()))
+				newList.add(evaluation);
+		return newList;
+	}
+
+	
+	
+	
+	private List<String> findCoursesWhereStudent() {
+		Map<String, Integer> allCourses = App.getInstance().getLoggedUser().getMyCourses();
+		List<String> relevantCourses = new ArrayList<String>();
+		for (Entry<String, Integer> entry : allCourses.entrySet()) {
+			String course = entry.getKey();
+			Integer role = entry.getValue();
+			if (role == 1)
+				relevantCourses.add(course);
+		}
+		return relevantCourses;
+	}
+	
 	public void returnHandler(javafx.event.ActionEvent event) {
-		App.getInstance().gotoStudentPage();
+		//TODO: App.getInstance().gotoPrevious();
 	}
 
 }
