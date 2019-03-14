360dba1f621151dcabb9aa8c98dab8f2954d8704
mariagrohshennig <mariagrohshennig@dhcp-10-20-132-144.wlan.ntnu.no>
2019-03-14 15:15:49 +0100

    endret navn på fxml til eval

--- src/main/java/gui/controllers/StudentAddAssignmentPageController.java
+++ src/main/java/gui/controllers/StudentAddAssignmentPageController.java
@@ -3,14 +3,28 @@ package gui.controllers;
 import gui.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-public class ViewAssignmentsPageController {
+public class StudentAddAssignmentPageController {
 	
 	@FXML
 	Button return_handler;
 	
+	@FXML
+	Button upload_assignment;
+	
+	@FXML 
+	TextField course_code;
+	
+	@FXML 
+	TextField assignment_name;
+	
+	@FXML 
+	Button add_file_handler;
+	
 	public void returnHandler(javafx.event.ActionEvent event) {
 		App.getInstance().gotoStudentPage();
 	}
+	
 
 }
--- src/main/java/gui/controllers/StudentViewAssignmentsPageController.java
+++ src/main/java/gui/controllers/StudentViewAssignmentsPageController.java
@@ -3,25 +3,11 @@ package gui.controllers;
 import gui.App;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 
-public class AddAssignmentPageController {
+public class StudentViewAssignmentsPageController {
 	
 	@FXML
 	Button return_handler;
-	
-	@FXML
-	Button upload_assignment;
-	
-	@FXML 
-	TextField course_code;
-	
-	@FXML 
-	TextField assignment_name;
-	
-	
-	@FXML 
-	Button add_file_handler;
 	
 	public void returnHandler(javafx.event.ActionEvent event) {
 		App.getInstance().gotoStudentPage();
