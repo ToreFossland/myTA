c69da57351f42f622944933392dd84af40f7b2d2
mariagrohshennig <mariagrohshennig@dhcp-10-22-15-117.wlan.ntnu.no>
2019-03-20 10:45:14 +0100

    timeshedule-klassen

--- src/main/java/timeshedule/TimeShedule.java
+++ src/main/java/timeshedule/TimeShedule.java
@@ -1,9 +1,34 @@
 package timeshedule;
 
+import java.util.ArrayList;
+
+import halltimes.Booking;
+import user.User;
+
 public class TimeShedule {
 	
 	
+	private ArrayList<Booking> whereStudent; // listen over bookinger hvor han er student
+	private ArrayList<Booking> whereTA;
+	private User user;
 	
+	public TimeShedule(User user, ArrayList<Booking> bookings) {
+		setUser(user);
+		addBookings(bookings);
+	}
 	
-
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public void addBookings(ArrayList<Booking> bookings) {
+		for (Booking booking:bookings) {
+			if (booking.getEmailStudent() == user.getEmail()) {
+				whereStudent.add(booking);
+			}
+			else { whereTA.add(booking);	
+			}
+		}
+	}
+	
 }
